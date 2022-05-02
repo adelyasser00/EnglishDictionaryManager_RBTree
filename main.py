fa973ab683bb06ec1ea83359034cc22403007ab6
adelyasser00 <adelyasser00@gmail.com>
2022-05-02 19:34:37 +0200

    Fixed: Duplicate words in tree

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from EngDict import EngDict
 file = open("EN-US-Dictionary.txt", "r")
 dictionary = EngDict()
 dictionary.LoadDictionary("EN-US-Dictionary.txt")
-#dictionary.InsertWord("hayam")
-
-dictionary.LoadDictionary("EN-US-Dictionary.txt")
-
+# dictionary.InsertWord("hayam")
 dictionary.RBtree.inOrderTraversal(dictionary.RBtree.root)
+print("Size of dictionary= " + str(dictionary.RBtree.size))
+print("Height of dictionary's tree= " + str(dictionary.RBtree.height(dictionary.RBtree.root)))
